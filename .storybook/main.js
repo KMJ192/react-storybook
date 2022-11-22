18ad74533b35ca4a8e5cc9228c26a8d28726f84d
MyeongJunKim <myeongjun222@naver.com>
2022-11-22 22:46:43 +0900

    프로젝트 셋팅

---
+++ .storybook/main.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
+module.exports = {
+  stories: ['../src/**/*.stories.mdx', '../**/*.stories.tsx'],
+  addons: [],
+  core: {
+    builder: '@storybook/builder-vite',
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  viteFinal: async (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@src': path.resolve(__dirname, 'src'),
+    };
+
+    return config;
+  },
+  // webpackFinal: async (config) => {
+  //   config.module.rules.push({
+  //     test: /\.(sass|s?css)$/,
+  //     use: [
+  //       'style-loader',
+  //       {
+  //         loader: 'css-loader',
+  //         options: {
+  //           modules: true,
+  //           importLoaders: 1,
+  //         },
+  //       },
+  //       'resolve-url-loader',
+  //       'sass-loader',
+  //     ],
+  //     include: /\.(module)\.(sass|s?css)$/i,
+  //   });
+
+  //   config.module.rules.push({
+  //     test: /\.(ts|tsx)$/,
+  //     loader: require.resolve('babel-loader'),
+  //     options: {
+  //       presets: [['react-app', { flow: false, typescript: true }]],
+  //     },
+  //   });
+
+  //   config.resolve.plugins.push(new TsconfigPathsPlugin({}));
+  //   config.resolve.alias = {
+  //     ...config.resolve.alias,
+  //     '@src': path.resolve(__dirname, 'src'),
+  //   };
+
+  //   return config;
+  // },
+};

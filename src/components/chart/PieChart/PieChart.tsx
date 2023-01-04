2728a719188349ade5358aaa1b3da3e38081dd1b
MyeongJunKim <myeongjun222@naver.com>
2023-01-04 23:15:37 +0900

    Update PieChart.tsx

--- src/components/chart/PieChart/PieChart.tsx
+++ src/components/chart/PieChart/PieChart.tsx
@@ -1,45 +1,56 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, forwardRef } from 'react';
 
-import type { Properties as CSSProperties } from 'csstype';
+import type { Properties as CSSType } from 'csstype';
 
 import classNames from 'classnames/bind';
 import style from './PieChart.module.scss';
 const cx = classNames.bind(style);
 
-type Props = {
-  type: 'canvas' | 'css';
-
-  // customStyle?: CSSProperties;
-};
+// type Props = {
+//   type: 'canvas' | 'css';
+//   // customStyle?: CSSProperties;
+// };
 
 // https://www.freecodecamp.org/news/css-only-pie-chart/
 
-function PieChart({ type }: Props) {
+// function PieChart({ type }: Props) {
+//   // import('@wasm/pkg').then(async (wasm) => {
+//   //   (await wasm.default()).console();
+//   // });
+//   const chartRef = useRef<HTMLDivElement | null>(null);
+
+//   useEffect(() => {
+//     const chart = chartRef.current;
+//     if (chart) {
+//       chart.style.setProperty('--per', '60');
+//       chart.style.setProperty('--bold', '10px');
+//       chart.style.setProperty('--data-color', 'skyblue');
+//       chart.style.setProperty('--width', '140px');
+//       chart.style.setProperty('--null-color', 'gray');
+//     }
+//   }, []);
+
+//   return (
+//     <div className={cx('chart-body')}>
+//       <div ref={chartRef} className={cx('pie-chart', 'animation')} style={{}}>
+//         60%
+//       </div>
+//       <div className={cx('chart-back')}></div>
+//     </div>
+//   );
+// }
+
+type Props = {
+  id: string;
+  style: CSSType;
+};
+
+const PieChart = forwardRef<HTMLDivElement, Props>(({ id, style }, ref) => {
   // import('@wasm/pkg').then(async (wasm) => {
   //   (await wasm.default()).console();
   // });
-  const chartRef = useRef<HTMLDivElement | null>(null);
-
-  useEffect(() => {
-    const chart = chartRef.current;
-    if (chart) {
-      chart.style.setProperty('--per', '60');
-      chart.style.setProperty('--bold', '10px');
-      chart.style.setProperty('--data-color', 'skyblue');
-      chart.style.setProperty('--width', '140px');
-      chart.style.setProperty('--null-color', 'gray');
-    }
-  }, []);
-
-  return (
-    <div className={cx('chart-body')}>
-      <div ref={chartRef} className={cx('pie-chart', 'animation')} style={{}}>
-        60%
-      </div>
-      <div className={cx('chart-back')}></div>
-    </div>
-  );
-}
+  return <div id={id} style={style} ref={ref}></div>;
+});
 
 PieChart.defaultProps = {};
 
